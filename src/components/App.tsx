462fcc34ce10088b239847d5847ff15a982a5c2d
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-07-25 22:59:41 +0430

    setup depedependencies

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>Hello from App Components</div>
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>Hello world!</h1>
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+import 'preline'
+import '@/main.css'
+
 import ReactDOM from 'react-dom'
 
 import App from '@/components/App'
