59e3515c18f144079e37161bc11de1b1d3fb4e9d
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-07-27 21:38:16 +0430

    Adding Navbar Component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,28 +1,47 @@
 import styles from './navbar.module.css'
 
-import { DarkThemeToggle } from 'flowbite-react'
+import { DarkThemeToggle, Avatar, TextInput, Navbar } from 'flowbite-react'
 import { HTMLProps } from 'react'
 import { twMerge } from 'tailwind-merge'
+
+import { AiOutlineSearch } from 'react-icons/ai'
 
 interface NavbarProps {
   className?: HTMLProps<HTMLElement>['className']
 }
 
-const Navbar: React.FC<NavbarProps> = ({ className }) => {
+const Nav: React.FC<NavbarProps> = ({ className }) => {
   return (
-    <nav className={twMerge(styles.root, className)} aria-label="Global">
-      <span className={styles.brand}>Filmpire</span>
+    <Navbar fluid rounded className={twMerge(styles.root, className)}>
+      <Navbar.Toggle />
 
-      <div className={styles.linkContainer}>
-        <DarkThemeToggle />
+      <TextInput
+        id="base"
+        sizing="md"
+        type="text"
+        color="gray"
+        placeholder="Search"
+        rightIcon={AiOutlineSearch}
+      />
 
-        <span className={styles.active} aria-current="page">
-          Landing
-        </span>
-        <span className={styles.link}>Account</span>
-      </div>
-    </nav>
+      <Navbar.Collapse className="bg-gray-dark md:bg-transparent z-0 rounded-md mt-4 md:mt-0">
+        <Navbar.Link className="md:mt-3" href="/movies">
+          Movies
+        </Navbar.Link>
+        <Navbar.Link>
+          <DarkThemeToggle className="rounded-full" />
+        </Navbar.Link>
+        <Navbar.Link>
+          <Avatar
+            bordered
+            alt="avatar of Alireza Mohseni"
+            img="https://t3.ftcdn.net/jpg/03/42/99/68/360_F_342996846_tHMepJOsXWwbvMpG7uiYpE68wbfQ9e4s.jpg"
+            rounded
+          />
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
-export default Navbar
+export default Nav
