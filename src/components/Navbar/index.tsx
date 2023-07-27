3e51e2a110b36cdcafa000cb5accc436a8cf0046
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-07-28 00:46:06 +0430

    Adding Zustand store for sidebar value

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,8 +10,9 @@ import {
 } from 'flowbite-react'
 import { twMerge } from 'tailwind-merge'
 
-import { HiSearch, HiMenu } from 'react-icons/hi'
-// import { Drawer } from '@/components'
+import { HiSearch, HiMenu, HiOutlineChevronDoubleLeft } from 'react-icons/hi'
+
+import { useSidebarStore } from '@/store/useSidebarStore'
 
 interface NavbarProps {
   className?: HTMLProps<HTMLElement>['className']
@@ -22,35 +23,32 @@ export const Navbar: React.FC<NavbarProps> = ({
   className,
   onSidebarClick,
 }) => {
+  const isSidebarOpen = useSidebarStore((state) => state.isSidebarOpen)
+
   return (
-    <>
-      {/* <Drawer open={mobileOpen} setOpen={DrawerToggle} className="bg-blue-700">
-        saddf
-      </Drawer> */}
-      <Nav fluid className={twMerge(styles.root, className)}>
-        <button onClick={onSidebarClick}>
-          <HiMenu />
-        </button>
+    <Nav fluid className={twMerge(styles.root, className)}>
+      <button onClick={onSidebarClick}>
+        {!isSidebarOpen ? <HiMenu /> : <HiOutlineChevronDoubleLeft />}
+      </button>
 
-        <TextInput
-          id="base"
-          sizing="md"
-          type="text"
-          placeholder="Search"
-          rightIcon={HiSearch}
+      <TextInput
+        id="base"
+        sizing="md"
+        type="text"
+        placeholder="Search"
+        rightIcon={HiSearch}
+      />
+
+      <div className="hidden md:flex gap-4">
+        <DarkThemeToggle className="rounded-full" />
+
+        <Avatar
+          bordered
+          alt="avatar of Alireza Mohseni"
+          img="https://t3.ftcdn.net/jpg/03/42/99/68/360_F_342996846_tHMepJOsXWwbvMpG7uiYpE68wbfQ9e4s.jpg"
+          rounded
         />
-
-        <div className="hidden md:flex gap-4">
-          <DarkThemeToggle className="rounded-full" />
-
-          <Avatar
-            bordered
-            alt="avatar of Alireza Mohseni"
-            img="https://t3.ftcdn.net/jpg/03/42/99/68/360_F_342996846_tHMepJOsXWwbvMpG7uiYpE68wbfQ9e4s.jpg"
-            rounded
-          />
-        </div>
-      </Nav>
-    </>
+      </div>
+    </Nav>
   )
 }
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,27 +1,24 @@
-import { useCallback, useState } from 'react'
 import styles from './layout.module.css'
 import { Drawer, Navbar } from '@/components'
+import { useSidebarStore } from '@/store/useSidebarStore'
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [mobileOpen, setMobileOpen] = useState<boolean>(false)
-
-  const DrawerToggle: () => void = useCallback(() => {
-    setMobileOpen((t) => !t)
-  }, [mobileOpen])
+  const isSidebarOpen = useSidebarStore((state) => state.isSidebarOpen)
+  const toggleSidebar = useSidebarStore((state) => state.toggleSidebar)
 
   return (
     <div className={styles.root}>
       <div className={styles.sidebarContainer}>
-        <Drawer open={mobileOpen} setOpen={DrawerToggle}>
+        <Drawer open={isSidebarOpen} setOpen={toggleSidebar}>
           Sidebar
         </Drawer>
       </div>
       <div className={styles.body}>
-        <Navbar className={styles.toolbar} onSidebarClick={DrawerToggle} />
+        <Navbar className={styles.toolbar} onSidebarClick={toggleSidebar} />
         <div className={styles.content}>{children}</div>
       </div>
     </div>
